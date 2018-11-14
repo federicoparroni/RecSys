52104ec6190147d329d34b7a6a81a2a9f9f8bcdb
Federico Parroni <keyblade95@live.it>
2018-11-14 17:51:42 +0100

    CollaborativeBM25 including scores in csv

--- algorithms/collaborative_BM25.py
+++ algorithms/collaborative_BM25.py
@@ -11,20 +11,17 @@ d = Data()
 targetUsersIds = d.target_playlists_df['playlist_id'].values
 
 # get item_user matrix by transposing the URM matrix
-URM = load_npz('../dataset/saved_matrices/sp_urm.npz')
 item_user_data = URM.transpose()
 print('> data loaded')
 
 # initialize a model (BM25 metric)
-
 model = implicit.nearest_neighbours.BM25Recommender(K=100, K1=0.8, B=0.3)
 
 # train the model on a sparse matrix of item/user/confidence weights
 model.fit(item_user_data)
 
 # build recommendations array
-recommendations = bridge.array_of_recommendations(model, target_user_ids=targetUsersIds, urm=URM)
+recommendations = bridge.array_of_recommendations(model, include_scores=True, target_user_ids=targetUsersIds, urm=URM)
 
 # export
-Export.export(np.array(recommendations), path='../submissions/', name='collaborative_BM25')
-print('file exported')
+print('> exported')
