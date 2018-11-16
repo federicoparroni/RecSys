dda46a392ae86f477dd0fae45700d022b253d02f
Federico Parroni <keyblade95@live.it>
2018-11-16 22:55:15 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="gabdampar_recommender",
+    version="0.0.1",
+    author="Gabdampar",
+    # author_email="author@example.com",
+    description="Another recommender system algorithms collection",
+    url="https://github.com/pypa/sampleproject",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'similaripy'
+    ]
+)
