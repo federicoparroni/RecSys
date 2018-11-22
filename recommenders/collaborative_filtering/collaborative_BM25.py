c168bad94db6d5e3a01c726db5e281aef21c9d65
Federico Parroni <keyblade95@live.it>
2018-11-22 10:41:39 +0100

    Added CollaborativeFiltering recommender. Removed some run files

--- recommenders/collaborative_filtering/collaborative_BM25.py
+++ recommenders/collaborative_filtering/collaborative_BM25.py
@@ -1,10 +1,6 @@
 import numpy as np
 from scipy.sparse import load_npz
 import implicit
-from implicit.evaluation import mean_average_precision_at_k
-from evaluation.map_evaluation import evaluate_map
-from recommenders import model_bridge as bridge
-from inout.export_rec import Export
 import pandas as pd
 
 
@@ -25,14 +21,10 @@ model.fit(item_user_data)
 # build recommendations array
 recommendations = bridge.array_of_recommendations(model, target_user_ids=targetUsersIds, urm=URM)
 
-
 # VALIDATION: load URM test MAP matrix
 test_urm = load_npz('raw_data/matrices/sp_urm_test_MAP.npz')
 map10 = evaluate_map(recommendations, test_urm)
 print('Estimated map --> {}'.format(map10))
-
-map10implicit = mean_average_precision_at_k(model, train_user_items=URM, test_user_items=test_urm)
-print('Library evaluation: {}'.format(map10implicit))
 
 Export.export(np.array(recommendations), path='../submissions/', name='BM25 K {} K1 {} B{}'.format(K, K1, B))
 
