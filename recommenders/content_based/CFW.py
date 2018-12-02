bb627e096f01940db1648044fa3a673368043b01
Unknown <giovanni.gabbolini@gmail.com>
2018-12-02 21:30:47 +0100

    really clean CFW

--- recommenders/content_based/CFW.py
+++ recommenders/content_based/CFW.py
@@ -7,86 +7,73 @@ Created on 08/09/17
 """
 
 from recommenders.recommender_base import RecommenderBase
-from recommenders.distance_based_recommender import DistanceBasedRecommender
 from scipy.sparse import linalg
-from scipy.sparse import diags
 from scipy.sparse import csr_matrix
 import time, sys
 import numpy as np
 import pyximport
 import similaripy as sim
+from recommenders.collaborative_filtering.itembased import CFItemBased
+from recommenders.distance_based_recommender import DistanceBasedRecommender
+from recommenders.content_based.content_based import ContentBasedRecommender
+import data.data as d
+from utils import log
+from inout import importexport
 pyximport.install(setup_args={"script_args":[],
                               "include_dirs":np.get_include()},
                   reload_support=True)
 
 class CFW(RecommenderBase):
+    """
+    it mixes a item cf similarity matrix with a cb similarity matrix by learning a set of weight for the icm
+    that push the cb similarity values towards the ones of the cf (for the values in common)
+    it is meant as a way to push the content based performances up
+
+    based on: CFW: A Collaborative Filtering System Using Posteriors Over Weights Of Evidence
+    """
 
     RECOMMENDER_NAME = "CFW_D_Similarity_Linalg"
 
-    def __init__(self, ICM, URM_train, S_matrix_target, S_matrix_contentKNN):
-        self.URM_train = None
-        self.sparse_weights = True
-        self.normalize = False
-        self.filterTopPop = False
-        self.filterTopPop_ItemsID = np.array([], dtype=np.int)
-        self.items_to_ignore_flag = False
-        self.items_to_ignore_ID = np.array([], dtype=np.int)
+    def __init__(self):
+        self.name = "CFW_D_Similarity_Linalg"
 
-        if(S_matrix_target.shape[0] != S_matrix_target.shape[1]):
-            raise ValueError("Items imilarity matrix is not square: rows are {}, columns are {}".format(S_matrix_target.shape[0],
-                                                                                                        S_matrix_target.shape[1]))
+        # best item based similarity collaborative filter
+        item = CFItemBased()
+        sim_item = item.fit(d.get_urm(), 600,
+                            distance=DistanceBasedRecommender.SIM_SPLUS,
+                            shrink=10,
+                            alpha=0.25,
+                            beta=0.5,
+                            l=0.25,
+                            c=0.5).tocsr()
+        # normalization, matrix similarity values are now among 0 and 1. little push in performances
+        self.S_matrix_target = sim_item/sim_item.max()
 
-        self.URM_train = URM_train
-        self.ICM = ICM
-        self.S_matrix_target = S_matrix_target
-        self.S_matrix_contentKNN = S_matrix_contentKNN
-
-        self.n_items = self.URM_train.shape[1]
-        self.n_users = self.URM_train.shape[0]
-        self.n_features = self.ICM.shape[1]
-
-    def recommend_batch(self, userids, N=10, urm=None, filter_already_liked=True, with_scores=False,
-                        items_to_exclude=[], verbose=False):
-        user_profile_batch = self.URM_train[userids]
-        scores_array = user_profile_batch.dot(self.W_sparse).toarray()
-
-        if filter_already_liked:
-            scores_array[user_profile_batch.nonzero()] = -np.inf
-
-        if len(items_to_exclude) > 0:
-            raise NotImplementedError('Items to exclude functionality is not implemented yet')
-
-        i = 0
-        l = []
-        for row_index in range(scores_array.shape[0]):
-            scores = scores_array[row_index]
-
-            relevant_items_partition = (-scores).argpartition(N)[0:N]
-            relevant_items_partition_sorting = np.argsort(-scores[relevant_items_partition])
-            ranking = relevant_items_partition[relevant_items_partition_sorting]
-            if with_scores:
-                s = scores_array[row_index, ranking]
-                l.append([userids[row_index]] + [list(zip(list(ranking), list(s)))])
-            else:
-                l.append([userids[row_index]] + list(ranking))
-            if verbose:
-                 i += 1
-                 # log.progressbar(i, scores_array.shape[0], prefix='Building recommendations ')
-
-        return l
+        # best similarity content based
+        content = ContentBasedRecommender()
+        sim_content = content.fit(d.get_urm(),
+                                  d.get_icm(),
+                                  k=100,
+                                  distance=DistanceBasedRecommender.SIM_SPLUS,
+                                  shrink=500,
+                                  alpha=0.5,
+                                  beta=1,
+                                  l=0.5,
+                                  c=0.5).tocsr()
+        # normalization, matrix similarity values are now among 0 and 1. little push in performances
+        self.S_matrix_contentKNN = sim_content/sim_content.max()
 
     def _generateTrainData_low_ram(self):
         print(self.RECOMMENDER_NAME + ": Generating train data")
         start_time_batch = time.time()
 
-        self._writeLog(self.RECOMMENDER_NAME + ": Collaborative S density: {:.2E}, nonzero cells {}".format(
+        print(self.RECOMMENDER_NAME + ": Collaborative S density: {:.2E}, nonzero cells {}".format(
             self.S_matrix_target.nnz/self.S_matrix_target.shape[0]**2, self.S_matrix_target.nnz))
 
-        self._writeLog(self.RECOMMENDER_NAME + ": Content S density: {:.2E}, nonzero cells {}".format(
+        print(self.RECOMMENDER_NAME + ": Content S density: {:.2E}, nonzero cells {}".format(
             self.S_matrix_contentKNN.nnz/self.S_matrix_contentKNN.shape[0]**2, self.S_matrix_contentKNN.nnz))
 
         if self.normalize_similarity:
-            # Compute sum of squared
             sum_of_squared_features = np.array(self.ICM.T.power(2).sum(axis=0)).ravel()
             sum_of_squared_features = np.sqrt(sum_of_squared_features)
 
@@ -165,7 +152,7 @@ class CFW(RecommenderBase):
 
                 start_time_batch = time.time()
 
-        self._writeLog(self.RECOMMENDER_NAME + ": Content S structure has {} out of {} ( {:.2f}%) nonzero collaborative cells".format(
+        print(self.RECOMMENDER_NAME + ": Content S structure has {} out of {} ( {:.2f}%) nonzero collaborative cells".format(
             num_common_coordinates, self.S_matrix_contentKNN.nnz, num_common_coordinates/self.S_matrix_contentKNN.nnz*100))
 
         # Discard extra cells at the left of the array
@@ -179,14 +166,45 @@ class CFW(RecommenderBase):
         collaborative_nnz = self.S_matrix_target.nnz
         collaborative_sum = sum(self.S_matrix_target.data)
 
-        self._writeLog(self.RECOMMENDER_NAME + ": Nonzero collaborative cell sum is: {:.2E}, average is: {:.2E}, "
+        print(self.RECOMMENDER_NAME + ": Nonzero collaborative cell sum is: {:.2E}, average is: {:.2E}, "
                       "average over all collaborative data is {:.2E}".format(
                       data_sum, data_sum/data_nnz, collaborative_sum/collaborative_nnz))
 
-    def fit(self, loss_tolerance = 1e-6, add_zeros_quota = 1, normalize_similarity = False,
-            iteration_limit = 30, damp_coeff=1):
+    def _compute_W_sparse(self):
+        if self.use_incremental:
+            feature_weights = self.D_incremental
+        else:
+            feature_weights = self.D_best
+
+        feature_weights = np.sqrt(feature_weights)
+        ICM_weighted = self.ICM * csr_matrix((feature_weights,
+                                              (np.arange(len(feature_weights), dtype=np.int32),
+                                               np.arange(len(feature_weights), dtype=np.int32))))
+
+        # TODO: insert here the optimal content based params
+        # db = DistanceBasedRecommender()
+        # self.W_sparse = db.fit(ICM_weighted,
+        #                        distance=DistanceBasedRecommender.SIM_JACCARD,
+        #                        k=1000,
+        #                        shrink=1000,
+        #                        alpha=0.5,
+        #                        beta=0.5,
+        #                        l=0.5,
+        #                        c=0.5).tocsr()
+        # self.W_sparse = ICM_weighted * ICM_weighted.T
+
+        self.W_sparse = sim.dot_product(ICM_weighted).tocsr()
+
+    def fit(self, ICM=None, URM_train=None, normalize_similarity = False, add_zeros_quota = 1, loss_tolerance = 1e-6,
+            iteration_limit = 30, damp_coeff=1, use_incremental=False):
+        self.URM_train = URM_train
+        self.ICM = ICM
+        self.n_items = self.URM_train.shape[1]
+        self.n_users = self.URM_train.shape[0]
+        self.n_features = self.ICM.shape[1]
         self.normalize_similarity = normalize_similarity
         self.add_zeros_quota = add_zeros_quota
+        self.use_incremental = use_incremental
 
         self._generateTrainData_low_ram()
 
@@ -198,89 +216,130 @@ class CFW(RecommenderBase):
         self.D_incremental = linalg_result[0].copy()
         self.D_best = linalg_result[0].copy()
         self.epochs_best = 0
-
         self.loss = linalg_result[3]
 
-        self._compute_W_sparse(use_incremental=False)
-        a = 1
+        self._compute_W_sparse()
 
-    def _writeLog(self, string):
-        print(string)
+    def recommend_batch(self, userids, N=10, urm=None, filter_already_liked=True, with_scores=False,
+                        items_to_exclude=[], verbose=False):
+        user_profile_batch = self.URM_train[userids]
+        scores_array = user_profile_batch.dot(self.W_sparse).toarray()
 
-    def _compute_W_sparse(self, use_incremental = False):
+        if filter_already_liked:
+            scores_array[user_profile_batch.nonzero()] = -np.inf
 
-        if use_incremental:
-            feature_weights = self.D_incremental
-        else:
-            feature_weights = self.D_best
+        if len(items_to_exclude) > 0:
+            raise NotImplementedError('Items to exclude functionality is not implemented yet')
 
-        feature_weights = np.sqrt(feature_weights)
-        ICM_weighted = self.ICM * csr_matrix((feature_weights,
-                                             (np.arange(len(feature_weights), dtype=np.int32),
-                                              np.arange(len(feature_weights), dtype=np.int32))))
+        i = 0
+        l = []
+        for row_index in range(scores_array.shape[0]):
+            scores = scores_array[row_index]
 
-        # TODO: insert here the optimal content based params
+            relevant_items_partition = (-scores).argpartition(N)[0:N]
+            relevant_items_partition_sorting = np.argsort(-scores[relevant_items_partition])
+            ranking = relevant_items_partition[relevant_items_partition_sorting]
+            if with_scores:
+                s = scores_array[row_index, ranking]
+                l.append([userids[row_index]] + [list(zip(list(ranking), list(s)))])
+            else:
+                l.append([userids[row_index]] + list(ranking))
+            if verbose:
+                 i += 1
+                 log.progressbar(i, scores_array.shape[0], prefix='Building recommendations ')
 
-        # db = DistanceBasedRecommender()
-        # self.W_sparse = db.fit(ICM_weighted,
-        #                        distance=DistanceBasedRecommender.SIM_JACCARD,
-        #                        k=1000,
-        #                        shrink=1000,
-        #                        alpha=0.5,
-        #                        beta=0.5,
-        #                        l=0.5,
-        #                        c=0.5).tocsr()
-
-        # self.W_sparse = ICM_weighted * ICM_weighted.T
-        # 0.3490
-        self.W_sparse = sim.dot_product(ICM_weighted).tocsr()
+        return l
 
     def get_r_hat(self):
         user_profile_batch = self.URM_train[d.get_target_playlists()]
-        return user_profile_batch.dot(self.W_sparse).toarray()
+        return user_profile_batch.dot(self.W_sparse)
 
     def recommend(self, userid, N=10, urm=None, filter_already_liked=True, with_scores=False, items_to_exclude=[]):
-        pass
+        return self.recommend_batch([userid],
+                                    N=N,
+                                    urm=urm,
+                                    filter_already_liked=filter_already_liked,
+                                    with_scores=with_scores,
+                                    items_to_exclude=items_to_exclude)
 
-    def run(self):
-        pass
+    def _print(self, normalize_similarity = False, add_zeros_quota = 1, loss_tolerance = 1e-6,
+            iteration_limit = 30, damp_coeff=1, use_incremental=False):
+        print('{} normalize_similarity: {}, add_zeros_quota: {}, loss_tolerance: {}, iteration_limit: {}, damp_coeff: {}, use_incremental: {}'.format(
+            self.name,
+            normalize_similarity,
+            add_zeros_quota,
+            loss_tolerance,
+            iteration_limit,
+            damp_coeff,
+            use_incremental
+        ))
 
+    def run(self, ICM, URM_train, normalize_similarity = False, add_zeros_quota = 1, loss_tolerance = 1e-6,
+            iteration_limit = 30, damp_coeff=1, use_incremental=False, export_results=True, export_r_hat=False):
+        self.fit(ICM=d.get_icm(),
+                 URM_train=d.get_urm(),
+                 normalize_similarity=normalize_similarity,
+                 add_zeros_quota=add_zeros_quota,
+                 loss_tolerance=loss_tolerance,
+                 iteration_limit=iteration_limit,
+                 damp_coeff=damp_coeff,
+                 use_incremental=use_incremental)
+        if export_results:
+            print('exporting results')
+            recs = self.recommend_batch(d.get_target_playlists(),
+                                         N=10,
+                                         urm=d.get_urm(),
+                                         filter_already_liked=True,
+                                         with_scores=False,
+                                         items_to_exclude=[],
+                                         verbose=False)
+            importexport.exportcsv(recs, 'submission', self._print(normalize_similarity=normalize_similarity,
+                                                                   add_zeros_quota=add_zeros_quota,
+                                                                   loss_tolerance=loss_tolerance,
+                                                                   iteration_limit=iteration_limit,
+                                                                   damp_coeff=damp_coeff,
+                                                                   use_incremental=use_incremental
+                                                                   ))
+        elif export_r_hat:
+            print('saving estimated urm')
+            self.save_r_hat()
 
-from recommenders.collaborative_filtering.itembased import CFItemBased
-from recommenders.distance_based_recommender import DistanceBasedRecommender
-from recommenders.content_based.content_based import ContentBasedRecommender
-import data.data as d
+    def validate(self, user_ids=d.get_target_playlists(), log_path=None, normalize_similarity = [False], damp_coeff=[1],
+                 add_zeros_quota = [1], loss_tolerance = [1e-6], iteration_limit = [30], use_incremental=[False]):
 
-# best collaborative item based
-item = CFItemBased()
-sim_item = item.fit(d.get_urm_train(), 600,
-                    distance=DistanceBasedRecommender.SIM_SPLUS,
-                    shrink=10,
-                    alpha=0.25,
-                    beta=0.5,
-                    l=0.25,
-                    c=0.5).tocsr()
-sim_item /= sim_item.max()
+        if log_path != None:
+            orig_stdout = sys.stdout
+            f = open(log_path + '/' + self.name + ' ' + time.strftime('_%H-%M-%S') + ' ' +
+                     time.strftime('%d-%m-%Y') + '.txt', 'w')
+            sys.stdout = f
 
-# best content
-content = ContentBasedRecommender()
-sim_content = content.fit(d.get_urm_train(),
-                          d.get_icm(),
-                          k=50,
-                          distance=DistanceBasedRecommender.SIM_SPLUS,
-                          shrink=50,
-                          alpha=0.5,
-                          beta=0.5,
-                          l=0.5,
-                          c=0.5).tocsr()
-sim_content /= sim_content.max()
+        for ns in normalize_similarity:
+            for dc in damp_coeff:
+                for adq in add_zeros_quota:
+                    for lt in loss_tolerance:
+                        for il in iteration_limit:
+                            for ui in use_incremental:
+                                print(self._print(normalize_similarity=ns,
+                                                  add_zeros_quota=dc,
+                                                  loss_tolerance=lt,
+                                                  iteration_limit=il,
+                                                  damp_coeff=dc,
+                                                  use_incremental=ui))
+                                self.fit(ICM=d.get_icm(),
+                                         URM_train=d.get_urm_train(),
+                                         normalize_similarity=ns,
+                                         add_zeros_quota=adq,
+                                         loss_tolerance=lt,
+                                         iteration_limit=il,
+                                         damp_coeff=dc,
+                                         use_incremental=ui)
 
-r = CFW(d.get_icm(),
-        d.get_urm_train(),
-        sim_item,
-        sim_content,
-        )
+                                recs = self.recommend_batch(user_ids, urm=d.get_urm_train())
+                                r.evaluate(recs, d.get_urm_test())
+        if log_path != None:
+            sys.stdout = orig_stdout
+            f.close()
 
-r.fit()
-recs = r.recommend_batch(d.get_target_playlists(), urm=d.get_urm_train())
-r.evaluate(recs, d.get_urm_test())
+#0.03571
+r = CFW()
+r.validate(log_path='.')
