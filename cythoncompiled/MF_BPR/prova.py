cbb50d1aef3f0662e00eb3a2f30ad405f3999646
giovanni <giovanni.gabbolini@gmail.com>
2018-12-13 07:25:38 -0500

    MFBPR baseline

--- cythoncompiled/MF_BPR/prova.py
+++ cythoncompiled/MF_BPR/prova.py
@@ -1,10 +1,10 @@
 from MF_BPR import fit_MFBPR
 import data.data as d
 r = fit_MFBPR()
-r.fit(d.get_urm_train(),
+r.fit(d.get_urm_train_1(),
       epochs=50,
       n_factors=150,
-      learning_rate=1e-1, 
-      user_regularization=1e-4,
-      positive_item_regularization=1e-4,
-      negative_item_regularization=1e-4)
+      learning_rate=1e-3, 
+      user_regularization=1e-3,
+      positive_item_regularization=1e-3,
+      negative_item_regularization=1e-3)
